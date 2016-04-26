112951827c25226d9c09bf5f842847678d3f3ae3
michaelisvy <misvy@gopivotal,com>
2016-04-26 06:53:04 +0530

    01c: updated `complete` project

--- sessions/day_01/session_01/lab_01c/complete/hello-spring-boot/src/test/java/io/spring/pivotal/HelloSpringBootApplicationTests.java
+++ sessions/day_01/session_01/lab_01c/complete/hello-spring-boot/src/test/java/io/spring/pivotal/HelloSpringBootApplicationTests.java
@@ -1,10 +1,10 @@
-package io.pivotal.spring.hello;
+package io.spring.pivotal;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = HelloSpringBootApplication.class)

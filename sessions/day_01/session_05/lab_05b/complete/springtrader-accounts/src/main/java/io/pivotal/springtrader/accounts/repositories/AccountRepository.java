7fc2b28368839951495ff4049c752c3405413bb5
michaelisvy <misvy@gopivotal,com>
2016-04-27 06:50:29 +0530

    Lab 05a - moved app.properties to app.yml + minor changes

--- sessions/day_01/session_05/lab_05b/complete/springtrader-accounts/src/main/java/io/pivotal/springtrader/accounts/repositories/AccountRepository.java
+++ sessions/day_01/session_05/lab_05b/complete/springtrader-accounts/src/main/java/io/pivotal/springtrader/accounts/repositories/AccountRepository.java
@@ -1,18 +1,14 @@
 package io.pivotal.springtrader.accounts.repositories;
 
-
-import io.pivotal.springtrader.accounts.domain.Account;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
+import io.pivotal.springtrader.accounts.domain.Account;
 
 public interface AccountRepository extends CrudRepository<Account,Integer> {
 
-	Account findByUseridAndPasswd(String userId, String passwd);
+    Account findByUseridAndPasswd(String userId, String passwd);
 
-	Account findByUserid(String userId);
+    Account findByUserid(String userId);
 
-	Account findByAuthtoken(String authtoken);
-
-	List<Account> findByFullnameIgnoreCase(String fullname);
+    Account findByAuthtoken(String authtoken);
 }
